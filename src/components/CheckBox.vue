230f17318547b37a0ea9c25fc527f5e8a2b2a8f2
Chih <forever040107@yahoo.com.tw>
2021-05-14 10:39:46 +0800

    Initial commit

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <Wrapper>
+    <input :name="name" type="checkbox" :checked="isChecked" @change="onChange">
+    <div class="label">change to Grid</div>
+  </Wrapper>
+</template>
+
+<script>
+import { Wrapper } from "./Styled.js";
+export default {
+  props: {
+    name: String,
+    isChecked: Boolean,
+    label: String
+  },
+  components: {
+    Wrapper
+  },
+  methods: {
+    onChange() {
+      console.log("#####");
+      this.$emit("change");
+    }
+  }
+};
+</script>
---
+++ src/components/Styled.js
@@ -0,0 +1,9 @@
+import styled from "vue-styled-components";
+
+export const Wrapper = styled("div")`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 10px;
+  justify-content: center;
+  align-items: center;
+`;
